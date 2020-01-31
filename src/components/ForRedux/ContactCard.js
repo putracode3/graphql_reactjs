a64a60ca5200dacd8c36fb2449cb0befaf95f606
Denny Setia W <dennysetiawisnugraha@gmail.com>
2020-01-31 11:34:31 +0700

    perbaikan view contact card

--- src/components/ForRedux/ContactCard.js
+++ src/components/ForRedux/ContactCard.js
@@ -3,13 +3,14 @@ import React from "react";
 const ContactCard = (contact) => {
   const ct = contact.contact;
   return (
-    <div>
+    <div className="row">
       <div className="col-xs-4 col-sm-3">
-          {ct.photo !== undefined ? <img src={ct.photo} alt={ct.email} className="img-fluid rounded-circle" /> : <img src="profile_img.png" width="30" alt="foto" className="img-fluid rounded-circle" />}
+          {ct.photo !== undefined ? <img src={ct.photo} alt={ct.email} className="img-fluid rounded-circle" /> : <img src="profile_img.png" width="50" alt="foto" className="img-fluid rounded-circle" />}
       </div>
       <div className="col-xs-8 col-sm-9">
-          <span className="name">{ct.email + ' ' + ct.address}</span><br/>
-          {/* Some code omitted for brevity */}
+          <span className="name">Email: {ct.email}</span>
+          <br/>
+          <span className="name">Address: {ct.address}</span>
       </div>
     </div>
   )
